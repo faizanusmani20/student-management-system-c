4bf4aa94f5d28f9256a1615087b6255c9ffb9ac6
benignUmbra <faizanusmani020@gmail.com>
2026-01-26 01:00:45 +0530

    Create alternate_way_to_addstudent.c

---
+++ alternate_way_to_addstudent.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct student s[100];
+int n = 0;
+
+void addStudent() {
+    printf("Enter Student Roll no: ");
+    scanf("%d", &s[n].rollno);
+
+    printf("Enter Student Name: ");
+    scanf("%s", s[n].name);
+
+    printf("Enter Student Marks: ");
+    scanf("%f", &s[n].marks);
+
+    n++;  // global variable updated
+    printf("Successfully Student Added\n");
+}
+int main(){
+addStudent();  // no need to pass n
+return 0;
